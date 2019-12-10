be09af8fb523600a3877a01b8a6fb2eb8e3dea92
DANIELCASTELAO\vmontestovar <vmontestovar@danielcastelao.org>
2019-12-10 10:52:36 +0100

    jogo

--- Boletin10_1/src/boletin10_1/Juego.java
+++ Boletin10_1/src/boletin10_1/Juego.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin10_1;
 
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author noese
- */
+
 public class Juego {
     public Juego(){
         
